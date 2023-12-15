7e47abfe80a5914a0f2713144f43b3cbdde518ec
Joe Hellerstein <jmh@berkeley.edu>
2023-12-14 18:29:08 -0800

    factor out the delta ops into modules

--- hydroflow/examples/dbsp/main.rs
+++ hydroflow/examples/dbsp/main.rs
@@ -2,133 +2,56 @@ use hydroflow::hydroflow_syntax;
 
 #[allow(non_snake_case)]
 pub fn main() {
-    // dT(x, id) :- Tin(...), a > 2;
-    // dR(id, y) :- Rin(...), s > 5;
-    // R@t+1 :- R@t;
-    // R@t+1 :- dR;
-    // T@t+1 :- T@t;
-    // T@t+1 :- dT;
-    // o_stream :- dR, dT;
-    // o_stream :- dR, T;
-    // o_stream :- R, dT;
     let (R_send, R_recv) = hydroflow::util::unbounded_channel::<((usize, usize, usize), i32)>();
     let (T_send, T_recv) = hydroflow::util::unbounded_channel::<((usize, usize, usize), i32)>();
     let mut df = hydroflow_syntax! {
-        Tin = source_stream(T_recv);
-        Rin = source_stream(R_recv);
-        // filter a > 2, project id, x
-        dT = Tin
+        // imports
+        delta_join = import!("delta_set_join_static.hf");
+        delta_distinct = import!("delta_distinct_static.hf");
+
+        source_stream(T_recv)
             -> filter(|((_, a, _), _)| a > &2)
-            -> map(|((id, _, x), n)| (id, x, n))
-            -> tee();
-
-        // filter s > 5, project id, y
-        dR = Rin
+            -> map(|((id, _, x), n)| ((id, (x)), n)) // ((key, tup), mult)
+            -> [0]delta_join;
+        source_stream(R_recv)
             -> filter(|((_, s, _), _)| s > &5)
-            -> map(|((id, _, y), m)| (id, y, m))
-            -> tee();
-
-        // count unique relational tuples from dT, generate tuples with multiplicities
-        T = dT
-            -> map(|(id, x, n): (usize, usize, i32)| ((id, x), n))
-            -> fold_keyed::<'static, (usize, usize), i32>(|| 0, |old: &mut i32, n: i32| *old += n) 
-            -> defer_tick()
-            //-> inspect(|t| println!("T: {:?}", t))
-            -> map(|((id, x), n)| (id, (x, n)))
-            -> [0]t_join;
-        // count unique relational tuples from dR, generate tuples with multiplicities
-        R = dR
-            -> map(|(id, y, m): (usize, usize, i32)| ((id, y), m))
-            -> fold_keyed::<'static, (usize, usize), i32>(|| 0i32, |old: &mut i32, m: i32| *old += m) 
-            -> defer_tick()
-            //-> inspect(|t| println!("R: {:?}", t))
-            -> map(|((id, y), m)| (id, (y, m)))
-            -> [1]r_join;
-
-        // ΔT × ΔR
-        dT -> map(|(id, x, n)| (id, (x, n))) -> [0]diffjoin;
-        dR -> map(|(id, y, m)| (id, (y, m))) -> [1]diffjoin;
-        diffjoin = join()
-            //-> inspect(|t| println!("d_join: {:?}", t))
-            -> o_stream;
-        // T × ΔR
-        dR -> map(|(id, y, m)| (id, (y, m))) -> [1]t_join;
-        t_join = join()
-            //-> inspect(|t| println!("t_join: {:?}", t))
-            -> o_stream;
-
-        // ΔT × R
-        dT -> map(|(k, x, n)| (k, (x, n))) -> [0]r_join;
-        r_join = join()
-            //-> inspect(|t| println!("r_join: {:?}", t))
-            -> o_stream;
-
-        // ΔT × ΔR + ΔT × R + T × ΔR ; new tuples from ΔT, ΔR
-        o_stream = union()
-            -> map(|(id, ((x, n), (y, m)))| ((id, x, y), n * m))
-            -> reduce_keyed::<'tick, (usize, usize, usize), i32>(|accum: &mut i32, m: i32| *accum += m)
-            -> map(|((id, x, y), m)| (id, x, y, m))
-            -> tee();
-
-        // I -> z^-1 ; accumulated state, delayed 1 tick
-        last_stream = o_stream
-            -> map(|(id, x, y, m)| ((id, x, y), m))
-            -> reduce_keyed::<'static, (usize, usize, usize), i32>(|accum: &mut i32, m: i32| *accum += m)
-            -> defer_tick();
-        // send to output if this tuple was not in last_output
-        o_stream
-            -> map(|(id, x, y, m)| ((id, x, y), m))
-            //-> inspect(|t| println!("ostream: {:?}", t))
-            -> [0]loj;
-        last_stream 
-            //-> inspect(|t| println!("last_stream: {:?}", t))  
-            -> [1]loj;
-        loj = import!("left_outer_join.hf");
-        // implment H operator: emit delta only if changing from positive to negative or vice versa
-        loj -> map(|((id, x, y), (current, last))| {
-                let last = last.unwrap_or(0);
-                let m = current + last;
-                if last > 0 && m <= 0 {
-                    ((id, x, y), -1)
-                } else if last <= 0 && m > 0 {
-                    ((id, x, y), 1)
-                } else {
-                    ((id, x, y), 0)
-                }
-            })
-            -> filter(|(_, m) | m != &0)
+            -> map(|((id, _, y), m)| ((id, (y)), m)) // ((key, tup), mult)
+            -> [1]delta_join;
+        delta_join
+            -> delta_distinct
             -> for_each(|t| println!("==> Output delta: {:?}", t));
     };
     df.run_available();
 
-    println!("\nStart tick 1");
-
+    // println!("\nStart tick 1");
+    // one new join tuple. delta: (0, 6, 10, 1)
     R_send.send(((0, 9, 10), 1)).unwrap();
     R_send.send(((1, 0, 1), 1)).unwrap();
     T_send.send(((0, 5, 6), 1)).unwrap();
     T_send.send(((1, 0, 1), 1)).unwrap();
     df.run_tick();
 
-    println!("\nStart tick 2");
-
-    R_send.send(((0, 9, 10), 1)).unwrap();
+    // println!("\nStart tick 2");
+    // 3 new, 1 old, no new output. cumu: (0, 6, 10, 4)
+    R_send.send(((0, 9, 10), 1)).unwrap(); // +1x1
     R_send.send(((1, 0, 1), 1)).unwrap();
-    T_send.send(((0, 5, 6), 1)).unwrap();
+    T_send.send(((0, 5, 6), 1)).unwrap(); // +1x2
     T_send.send(((1, 0, 1), 1)).unwrap();
     df.run_tick();
 
-    println!("\nStart tick 3");
-
-    R_send.send(((0, 9, 10), -1)).unwrap();
+    // println!("\nStart tick 3");
+    // -3 + 2 new, 4 old, no new output. cumu: (0, 6, 10, 3)
+    R_send.send(((0, 9, 10), -1)).unwrap(); // -1x2
     R_send.send(((1, 0, 1), 1)).unwrap();
-    T_send.send(((0, 5, 6), 1)).unwrap();
+    T_send.send(((0, 5, 6), 1)).unwrap(); // +1x2 + -1x1
     T_send.send(((1, 0, 1), 1)).unwrap();
     df.run_tick();
 
-    println!("\nStart tick 4");
-    R_send.send(((0, 9, 10), -1)).unwrap();
+    // println!("\nStart tick 4");
+    // -5 + 2 new, 3 old, deletion output. cumu: (0, 6, 10, 0))
+    R_send.send(((0, 9, 10), -1)).unwrap(); // -1x3
     R_send.send(((1, 0, 1), 1)).unwrap();
-    T_send.send(((0, 5, 6), 1)).unwrap();
+    T_send.send(((0, 5, 6), 1)).unwrap(); // +1x2 + -1x2
     T_send.send(((1, 0, 1), 1)).unwrap();
     df.run_tick();
 
@@ -142,5 +65,38 @@ pub fn main() {
 
 #[test]
 fn test() {
-    main();
+    use hydroflow::util::{run_cargo_example, wait_for_process_output};
+
+    let (_child, _, mut stdout) = run_cargo_example("dbsp", "");
+
+    let mut output = String::new();
+    // wait_for_process_output(&mut output, &mut stdout, "delta: ((0, 6, 10), 1)");
+    // wait_for_process_output(
+    //     &mut output,
+    //     &mut stdout,
+    //     r#"==> Output delta: ((0, 6, 10), 1)"#,
+    // );
+    // wait_for_process_output(
+    //     &mut output,
+    //     &mut stdout,
+    //     r#"last cumulative: ((0, 6, 10), 1)"#,
+    // );
+    // wait_for_process_output(&mut output, &mut stdout, r#"delta: ((0, 6, 10), 3)"#);
+    // wait_for_process_output(
+    //     &mut output,
+    //     &mut stdout,
+    //     r#"last cumulative: ((0, 6, 10), 4)"#,
+    // );
+    // wait_for_process_output(&mut output, &mut stdout, r#"delta: ((0, 6, 10), -1)"#);
+    // wait_for_process_output(
+    //     &mut output,
+    //     &mut stdout,
+    //     r#"last cumulative: ((0, 6, 10), 3)"#,
+    // );
+    // wait_for_process_output(&mut output, &mut stdout, r#"delta: ((0, 6, 10), -3)"#);
+    // wait_for_process_output(
+    //     &mut output,
+    //     &mut stdout,
+    //     r#"==> Output delta: ((0, 6, 10), -1)"#,
+    // );
 }
